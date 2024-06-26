5517364884615fd8ae87d896bd1e4eb56bba430f
muhnurulhakim <116804209+muhnurulhakim@users.noreply.github.com>
2024-06-26 07:21:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const themeToggle = document.getElementById('theme-toggle');
     const themeIcon = document.getElementById('theme-icon');
-    
-    // Check for saved theme preference or default to light theme
-    if (localStorage.getItem('theme') === 'dark') {
-        document.body.classList.add('dark-mode');
-        themeIcon.src = 'https://img.icons8.com/ios-filled/50/fff/moon-symbol.png';
-    }
 
     themeToggle.addEventListener('click', function() {
         document.body.classList.toggle('dark-mode');
@@ -15,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function() {
             themeIcon.src = 'https://img.icons8.com/ios-filled/50/fff/moon-symbol.png';
             localStorage.setItem('theme', 'dark');
         } else {
-            themeIcon.src = 'https://img.icons8.com/ios-filled/50/fff/sun.png';
+            themeIcon.src = 'https://img.icons8.com/ios-filled/50/000/sun.png';
             localStorage.setItem('theme', 'light');
         }
     });
